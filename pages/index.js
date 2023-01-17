2919254dd5bc376d0a15764b3370ab3c6a9db4c1
uvacoder <uva22940coder@gmail.com>
2023-01-17 12:58:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -66,12 +66,12 @@ const ExplorePage = () => (
         }}
         >
         <NavLink href="https://github.com/uvacoder">GitHub</NavLink>
-        <NavLink href="#">
-        <NavLink href="https://abclinks.vercel.app">MyLinks</NavLink>
-        <NavLink href="#">
-        <NavLink href="https://uvacoderportfolio.vercel.app">Portfolio</NavLink>
-        <NavLink href="#">
-        </NavLink>
+         <NavLink href="#">
+             Link 2
+         </NavLink>
+         <NavLink href="#">
+             Link 3
+         </NavLink>
         </Grid>
     </Container>
     </Box>
